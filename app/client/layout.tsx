a69b4c4c8c43c47f6373b2bc56c3d14946415181
AlexanderHait <ariz3n@yandex.ru>
2026-08-08 23:51:30 +0300

    Reuse cached client settings in the layout

--- app/client/layout.tsx
+++ app/client/layout.tsx
@@ -3,18 +3,14 @@ import ClientExperienceCenter from "@/components/ClientExperienceCenter";
 import ClientDesignV3 from "@/components/ClientDesignV3";
 import { requireClient } from "@/lib/auth";
 import { clientSetupComplete } from "@/lib/client-setup";
-import { getSupabaseAdmin } from "@/lib/supabase-admin";
+import { clientSettingsAccountData } from "@/lib/account-data";
 import { subscriptionAccess } from "@/lib/subscription-access";
 
 export default async function ClientLayout({ children }: { children: React.ReactNode }) {
   const current = await requireClient();
-  const db = getSupabaseAdmin();
-  const [access, { data: settings }] = await Promise.all([
+  const [access, settings] = await Promise.all([
     subscriptionAccess(current.accountId!),
-    db.from("client_settings")
-      .select("goal,current_weight_kg,kcal_target")
-      .eq("account_id", current.accountId!)
-      .maybeSingle(),
+    clientSettingsAccountData(current.accountId!),
   ]);
 
   return (
