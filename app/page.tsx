2d67b4cae9a6f65a06f26c75871a871889b36343
AlexanderHait <ariz3n@yandex.ru>
2026-08-09 00:40:25 +0300

    Ускорить открытие главного адреса
    Незалогиненный пользователь сразу попадает на вход без промежуточной загрузки личного кабинета.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default function Home() {
-  redirect("/client");
+export default async function Home() {
+  const sessionCookie = (await cookies()).get("ain_session")?.value;
+  redirect(sessionCookie ? "/client" : "/login");
 }
